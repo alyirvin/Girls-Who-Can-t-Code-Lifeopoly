825e2171b07ac6f1e354a13db0cd5cfb50a22d7b
Nat8722 <147213169+Nat8722@users.noreply.github.com>
2023-10-07 07:24:13 -0400

    card

---
+++ Assets/Player Cards/Increase10.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class Increase10 : MonoBehaviour
+{
+    public int playerMoneyInt1;
+    public TMP_Text MoneyAmount1;
+
+    [ContextMenu("Increase10 Money")]
+    public void addMoney()
+    {
+        playerMoneyInt1 = playerMoneyInt1 + 10000;
+        MoneyAmount1.text = playerMoneyInt1.ToString();
+    }
+
+    [ContextMenu("Decrease10 Money")]
+    public void lessMoney()
+    {
+        playerMoneyInt1 = playerMoneyInt1 - 10000;
+        MoneyAmount1.text = playerMoneyInt1.ToString();
+    }
+}
