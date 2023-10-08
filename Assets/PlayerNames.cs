87384c61b77b2c4eed4f4379b011df4674f99e5d
Nat8722 <147213169+Nat8722@users.noreply.github.com>
2023-10-08 04:34:03 -0400

    Revert "Revert "Merge branch 'main' of https://github.com/alyirvin/Girls-Who-Can-t-Code""
    This reverts commit 09740c6749a2df4dea134d6d78aaa9f0d6b5c747.

--- Assets/PlayerNames.cs
+++ Assets/PlayerNames.cs
@@ -3,17 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using System; 
 using PlayerSpace;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerNames : MonoBehaviour
 {
     private string input;
+    public GameObject NameInput;
+    public TMP_Text PlayerNumber;
+    public static int turn;
+    public TMP_Text NameFieldText;
+    public TMP_InputField NameInputField;
+    public Button P4;
+    public Button P3;
+    public Button P2;
+    public Button P1; 
+
     //public void createPlayer(string name);
     //public Player(string name);
 
     // Start is called before the first frame update
     void Start()
     {
-
+        turn = 0;
+        PlayerNumber.text = (turn+1).ToString();
     }
 
     // Update is called once per frame
@@ -24,11 +37,51 @@ public class PlayerNames : MonoBehaviour
 
     public void ReadInput(string s)
     {
-        input = s;
+        input = NameInputField.text;
         Debug.Log(input);
         (new PlayerMakers()).createPlayer(input);
+        if (turn < 4)
+        {
+            setPlayerToggle(input);
+        }
+        turn++;
+        NameInputField.text = "";
+        if (turn < 4)
+        {
+            nextPlayer();
+        }
     }
 
+    public void nextPlayer()
+    {
+        PlayerNumber.text = (turn+1).ToString();
+        NameInput.SetActive(!NameInput.activeInHierarchy);
+    }
+
+    public void ToggleNameInput()
+    {
+        NameInput.SetActive(!NameInput.activeInHierarchy);
+    }
+
+    public void setPlayerToggle(string name)
+    {
+        if (turn == 0)
+        {
+            P1.GetComponentInChildren<TMP_Text>().text = name;
+        }
+        else if (turn == 1)
+        {
+            P2.GetComponentInChildren<TMP_Text>().text = name;
+        }
+        else if (turn == 2)
+        {
+            P3.GetComponentInChildren<TMP_Text>().text = name;
+        }
+        else if (turn == 3)
+        {
+            P4.GetComponentInChildren<TMP_Text>().text = name;
+        }
+    }
 }
 
 namespace PlayerSpace
